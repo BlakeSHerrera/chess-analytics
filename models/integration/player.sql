1c81de19f81d236145afd0e740069268afd36ecb
Blake Herrera <blakeherrera1@gmail.com>
2026-06-10 23:56:35 -0500

    Bugfix player dim logic

--- models/integration/player.sql
+++ models/integration/player.sql
@@ -2,11 +2,7 @@ MODEL (
     NAME integration.player,
     KIND INCREMENTAL_BY_UNIQUE_KEY (
         UNIQUE_KEY (player),
-        WHEN_MATCHED (
-            WHEN MATCHED THEN UPDATE SET
-                created_at = LEAST(source.created_at, target.created_at),
-                is_bot = GREATEST(source.is_bot, target.is_bot))
-    ),
+        BATCH_SIZE @default_batch_size),
     GRAIN player);
 
 WITH players AS (
@@ -26,12 +22,23 @@ WITH players AS (
         t.utc_time
     FROM prep.game t
     WHERE t.utc_date BETWEEN @start_date AND @end_date
+),
+
+players_grouped AS (
+    SELECT
+        t.player,
+        MAX(t.title IS NOT NULL AND 'BOT' IN t.title) AS is_bot,
+        MIN(t.utc_date + t.utc_time) AS created_at
+    FROM players t
+    GROUP BY t.player
 )
 
-SELECT DISTINCT
-    @GENERATE_SURROGATE_KEY(t.player) AS player_key,
-    t.player,
-    MAX('BOT' IN t.title) AS is_bot,
-    MIN(t.utc_date + t.utc_time) AS created_at
-FROM players t
-GROUP BY t.player;
+SELECT
+    @GENERATE_SURROGATE_KEY(source.player)::TEXT AS player_key,
+    player::TEXT,
+    GREATEST(source.is_bot, target.is_bot)
+        ::BOOLEAN AS is_bot,
+    LEAST(source.created_at, target.created_at)::TIMESTAMP AS created_at
+FROM
+    players_grouped source
+    LEFT JOIN @this_model target USING (player);
